2a5ffce8dda654710bd6a279354bc766bcf245d0
amirazizi4861@gmail.com <amirazizi4861@gmail.com>
2020-10-19 16:35:34 +0800

    Summary

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,6 +8,21 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: 420,
+                child: const DecoratedBox(
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
